4f10a2649f32c1ee820932fa3a97cb5a9609c88a
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-03-30 15:18:41 +0400

    feat: add webpack, generateArticle object

---
+++ src/js/Article.js
@@ -0,0 +1,44 @@
+export class Article {
+    constructor({id, title, urlToImage, tags, ...rest}) {
+        this.id = id;
+        this.title = title;
+        this.urlToImage = urlToImage;
+        this.tags = tags;
+    }
+
+    //Article generator
+    generateArticle() {
+        let template = '' ;
+        let article = document.createElement('article');
+        article.className = 'strategy block-shadowed';
+        article.setAttribute('data-id', this.id);
+
+
+        this.urlToImage && //if(urlToImage)
+        (template += `<img class="strategy__image" src=${this.urlToImage} alt="strategy">`);
+
+        if(this.title || this.tags) {
+            template += `<div class="strategy__content">`
+
+                this.title && //if(title)
+                (template += `<h3 class="strategy__name" src=${this.title}></h3>`);
+
+                if(this.tags) {
+                    template += `<div class="strategy__tags tags">`
+
+                        this.tags.map(tag => {
+                            template += `<span class="tag tag_colored">${tag}</span>`
+                        })
+
+                    template += `</div>`
+                }
+
+            template += `</div>`
+        }
+
+        article.innerHTML = template;
+        return article;
+
+    }
+
+}
